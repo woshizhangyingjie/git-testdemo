e13e8fa55743b872fb5b68aeb13131a0fb97947e
zhang <zhang@123.com>
2022-03-28 10:34:27 +0800

    first commit

---
+++ src/main/java/com/zyj/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zyj.git;
+
+/**
+ * @Author zhang
+ * @Date 2022/3/28 - 10:15
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
